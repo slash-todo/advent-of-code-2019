175b164be24a3b2bb32231ac6bad6f65954c3079
KenavR <kenavr@gmail.com>
2019-12-02 19:21:19 +0100

    Implemented day 2 part one

--- 02/kenavr/src/main/java/aoc/kenavr/Main.java
+++ 02/kenavr/src/main/java/aoc/kenavr/Main.java
@@ -1,7 +1,15 @@
 package aoc.kenavr;
 
+import aoc.kenavr.parts.PartOne;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = Utils.loadInputs("input.txt");
+        numbers[1] = 12;
+        numbers[2] = 2;
+
+        PartOne one = new PartOne(numbers);
+        int[] result = one.run(0);
+        System.out.println(String.format("1) Calculated Value: %d", result[0]));
     }
 }
