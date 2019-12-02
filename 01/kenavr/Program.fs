9ac45b17a09c8513fc59c2d9c19c1972dee54ef1
KenavR <kenavr@gmail.com>
2019-12-02 14:51:00 +0100

    Implemented part one for day 1

--- 01/kenavr/Program.fs
+++ 01/kenavr/Program.fs
@@ -1,8 +1,18 @@
-﻿open System
+﻿namespace Aoc
+
+open System
 open System.IO
+
+open Aoc.PartOne
 
 module main =
   [<EntryPoint>]
   let main argv =
-      printfn "..."
+      let toInt (mass: String): int = mass |> int
+      let loadInputs: String[] = File.ReadAllLines(@"./input.txt") 
+      let inputs: int[] = loadInputs |> Array.map toInt
+
+      let requiredFuelEmpty = calcRequiredFuelEmpty inputs
+      printfn "1) Required fuel empty: %s" (requiredFuelEmpty |> string)
+
       0 // return an integer exit code
