065d5c27c317ef834ba34d4210455a4bd9c0e74b
Scott Robertson <scott.t.robertson@gmail.com>
2019-12-05 18:17:08 -0500

    unfinished day four solution

---
+++ 04/skat/04a.ts
@@ -0,0 +1,41 @@
+import inputs from './input';
+
+const min: number = 347312;
+const max: number = 805915;
+
+const possibilities: number[] = [];
+
+for (let i: number = min; i < max; i++) {
+    const string: string = i.toString(10);
+    const number: number = i;
+    const array: number[] = string.split('').map(str => Number(str));
+
+    // conditions
+    const isSixDigitNumber: boolean = string.split('').length === 6;
+    const isInRange: boolean = number > min && number < max;
+    const adjacentDigitsMatch: boolean = array.some(
+        (val, index, arr): boolean => arr[index] === arr[index + 1]
+    );
+    const isIncreasingSequence: boolean = array.every(
+        (val, index, arr): boolean => {
+            return (
+                arr[index] <= arr[index + 1] || arr[index + 1] === arr.length
+            );
+        }
+    );
+
+    if (adjacentDigitsMatch || isIncreasingSequence) {
+        debugger;
+    }
+    if (
+        isSixDigitNumber &&
+        isInRange &&
+        adjacentDigitsMatch &&
+        isIncreasingSequence
+    ) {
+        // if all conditions are true, add to the possibilities
+        possibilities.push(i);
+    }
+}
+
+console.log(possibilities.length);
