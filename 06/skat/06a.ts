8af21ac9e1679d3fe4a68ffc760b990a564ad6e6
Scott Robertson <scott.t.robertson@gmail.com>
2019-12-11 12:53:32 -0500

    super proud of this one =]

--- 06/skat/06a.ts
+++ 06/skat/06a.ts
@@ -1,6 +1,3 @@
-// unsolved
-
-// import inputs from './input';
 import inputs from '../../06/skat/input';
 
 const simpleInputs = [
@@ -19,34 +16,87 @@ const simpleInputs = [
 
 class System {
     bodies: Map<string, Body>;
-    com: Body | undefined;
+    com: Body;
+    systemGenerated: boolean;
 
     constructor(orbitalMap: string[]) {
+        this.systemGenerated = false;
         this.bodies = new Map();
         this.generateSystemMap(orbitalMap);
-        this.com = this.bodies.get('COM');
+        this.com = this.getBody('COM');
     }
 
     generateSystemMap(map: string[]): void {
         map.forEach((entry: string) => this.processMapEntry(entry));
+        this.systemGenerated = true;
     }
 
     processMapEntry(entry: string): void {
-        const self = entry.split(')')[0];
-        const child = entry.split(')')[1];
-        const body: Body | undefined = this.bodies.get(self);
+        const self: string = entry.split(')')[0];
+        const child: string = entry.split(')')[1];
+        const selfBody: Body | undefined = this.bodies.get(self);
+        const childBody: Body | undefined = this.bodies.get(child);
 
-        if (body) {
-            body.children.push(child);
-            this.bodies.set(self, body);
-        } else {
-            this.bodies.set(self, new Body(this, self, null, [child]));
-        }
+        const _placeSelf = () => {
+            if (selfBody) {
+                // body already exists
+                selfBody.children.push(child);
+                this.bodies.set(self, selfBody);
+            } else {
+                // body is new
+                this.bodies.set(self, new Body(this, self, [child]));
+            }
+        };
+
+        const _placeChild = () => {
+            if (!childBody) {
+                this.bodies.set(child, new Body(this, child));
+            }
+        };
+
+        _placeSelf();
+        _placeChild();
     }
-    findParent(name: string): Body | undefined {
-        return Array.from(this.bodies.values()).find(body =>
+    findParent(name: string): Body {
+        const target = Array.from(this.bodies.values()).find(body =>
             body.children.includes(name)
         );
+
+        if (!target) {
+            throw new Error(`Body name: '${name}' Not Found`);
+        }
+
+        return target;
+    }
+    getBody(name: string): Body {
+        const target = this.bodies.get(name);
+        if (!target) {
+            throw new Error(`Body name: '${name}' Not Found`);
+        }
+        return target;
+    }
+    getLineage(name: string): Body[] {
+        const starter: Body = this.getBody(name);
+        const lineage: Body[] = [];
+        let current: Body = starter;
+
+        if (!this.systemGenerated) {
+            throw new Error('System Not Generated');
+        }
+
+        while (current !== this.com && this.systemGenerated) {
+            lineage.unshift(current!);
+            current = current.parent();
+        }
+        lineage.unshift(this.getBody('COM')!);
+
+        return lineage;
+    }
+    countOrbits(): number {
+        const names = Array.from(this.bodies.keys());
+        return names.reduce((acc: number, cur: string): number => {
+            return (acc += this.getLineage(cur).length - 1);
+        }, 0);
     }
 }
 
@@ -54,21 +104,16 @@ class Body {
     system: System;
     name: string;
     children: string[];
-    constructor(
-        system: System,
-        name: string,
-        parent: string | null = null,
-        children: string[] = []
-    ) {
+    constructor(system: System, name: string, children: string[] = []) {
         this.system = system;
         this.name = name;
         this.children = children;
     }
-    parent(): Body | undefined {
+    parent(): Body {
         return this.system.findParent(this.name);
     }
 }
 
-console.log(new System(inputs));
-
+const system = new System(inputs);
+console.log(system.countOrbits());
 debugger;
